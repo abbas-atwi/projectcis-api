4d2c036997ac0906307a4b2fb77d6af2fb7ce540
Thiago Alves <thiago_marketingdigital@hotmail.com>
2020-04-28 20:16:36 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/br/uniamerica/cis/ProjectcisApiApplication.java
@@ -0,0 +1,13 @@
+package br.uniamerica.cis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcisApiApplication.class, args);
+	}
+
+}
